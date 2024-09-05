bdfead34d49b1d0251817215a54a35a9b5a4605e
LiamHsu <s459958@gmail.com>
2024-09-05 09:53:09 +0800

    Init xcode project

---
+++ WeatherAQI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAQI
+//
+//  Created by Liam on 2024/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAQI/WeatherAQIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAQIApp.swift
+//  WeatherAQI
+//
+//  Created by Liam on 2024/9/5.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAQIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
