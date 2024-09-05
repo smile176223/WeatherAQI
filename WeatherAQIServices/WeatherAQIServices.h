5570974f75f18a36acfb5692a6ef0183ce0bae3d
LiamHsu <s459958@gmail.com>
2024-09-05 10:09:41 +0800

    Set up project

---
+++ WeatherAQIServices/WeatherAQIServices.h
@@ -0,0 +1,18 @@
+//
+//  WeatherAQIServices.h
+//  WeatherAQIServices
+//
+//  Created by Liam on 2024/9/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherAQIServices.
+FOUNDATION_EXPORT double WeatherAQIServicesVersionNumber;
+
+//! Project version string for WeatherAQIServices.
+FOUNDATION_EXPORT const unsigned char WeatherAQIServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherAQIServices/PublicHeader.h>
+
+
